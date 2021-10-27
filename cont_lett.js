f3bffae3a33b1a60fadf323eb6fd20af626772f5
diffo23 <lizandro23difo@gmail.com>
2021-10-27 13:28:37 -0400

    add: Commets to problem cont words

--- cont_lett.js
+++ cont_lett.js
@@ -1,21 +1,36 @@
 
 const dato = "Hola todos";
 
-const contLet  = (letras) => {
+const contLet = (letras) => {
 
-const lett = {};
+    // letras -> Hola todos
 
-for(let letra of letras ) {
-    if(letra in lett) {
-        lett[letra] += 1;
-    } else {
-        lett[letra] = 1;
+    const lett = {};
+    // For of : Util para strings y arreglos
+    for (let letra of letras) {
+        if (letra in lett) {
+            // letra repetida
+            lett[letra] += 1;
+        } else {
+            // letra nueva
+            lett[letra] = 1;
+        }
     }
-}
 
-for(let letra in lett) {
-    console.log(`${letra} repite ${ lett[letra]}`);
-}
+    /*  H repite 1
+        o repite 3
+        l repite 1
+        a repite 1
+          repite 1
+        t repite 1
+        d repite 1
+        s repite 1
+    */
+    // For in: Util para objetos
+    for (let letra in lett) {
+        // Letra es la clave de mi objecto
+        console.log(`${letra} repite ${lett[letra]}`);
+    }
 
 }
 
