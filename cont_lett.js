ec6856eed6dc022ae84a49e44e218688c17356b1
diffo23 <lizandro23difo@gmail.com>
2021-10-27 12:16:32 -0400

    Contador de letras

--- cont_lett.js
+++ cont_lett.js
@@ -1,28 +1,21 @@
 
 const dato = "Hola todos";
 
-
 const contLet  = (letras) => {
 
 const lett = {};
 
-// "H" : 1
-
 for(let letra of letras ) {
-
     if(letra in lett) {
         lett[letra] += 1;
     } else {
         lett[letra] = 1;
     }
-
 }
 
 for(let letra in lett) {
-
     console.log(`${letra} repite ${ lett[letra]}`);
 }
-
 
 }
 
