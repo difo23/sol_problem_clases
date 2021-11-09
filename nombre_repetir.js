a79c32e4479d4ff777fc5e5b82f896600b2cc7f4
diffo23 <lizandro23difo@gmail.com>
2021-11-09 09:23:16 -0400

    feat: Resolve problems 1 y 2

---
+++ nombre_repetir.js
@@ -0,0 +1,14 @@
+// Start
+
+const num = 2;
+const nom = "Jose";
+
+
+// Ciclo
+for(let i=0; i < num; ++i){
+    // Imprimir nom
+    console.log(nom);
+}
+
+
+// End
---
+++ num_sum.js
@@ -0,0 +1,17 @@
+// Leer numero 
+let num = 9;
+let sum = 0;
+
+// not !
+while( !(num == 0)   ){
+    sum += num;
+    --num;
+}
+
+console.log(sum);
+
+
+// Gauss
+num = 9;
+let sum_gauss = num * (num + 1)/2
+console.log(sum_gauss);
